aa1c47e22e871d96046678c34ebd172843854f9d
artur <email@ggg.gg>
2025-05-16 13:11:06 +0200

    init

---
+++ src/main/java/com/empik/empiktask/EmpikTaskApplication.java
@@ -0,0 +1,13 @@
+package com.empik.empiktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpikTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/empik/empiktask/EmpikTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empik.empiktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
