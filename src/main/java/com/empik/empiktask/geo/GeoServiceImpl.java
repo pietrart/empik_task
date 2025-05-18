3f637b49524c0506e68715ed1570727703a0fcc7
artur <email@ggg.gg>
2025-05-18 12:59:42 +0200

    coupon ip cache refactor

--- src/main/java/com/empik/empiktask/geo/GeoServiceImpl.java
+++ src/main/java/com/empik/empiktask/geo/GeoServiceImpl.java
@@ -3,8 +3,9 @@ package com.empik.empiktask.geo;
 import com.empik.empiktask.common.CountryCode;
 import com.empik.empiktask.common.IpAddress;
 import com.empik.empiktask.common.error.TaskAppException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,30 +16,21 @@ import org.springframework.stereotype.Service;
 class GeoServiceImpl implements GeoService {
 
     private final GeoApiClient client;
-
-    private final Map<IpAddress, CountryCode> localCache = new ConcurrentHashMap<>();
     private static final int maxCacheSize = 50_000;
+    private final Cache<IpAddress, CountryCode> localCache = Caffeine.newBuilder()
+        .expireAfterWrite(1, TimeUnit.DAYS)
+        .maximumSize(maxCacheSize)
+        .build();
 
     @Override
     public CountryCode getCountryCodeByIp(IpAddress ip) {
         log.info("Resolve country code by ip: {}", ip.address());
-        clearCacheIfNeeded();
-        return localCache.computeIfAbsent(ip, ipAdr -> {
+        return localCache.get(ip, ipAdr -> {
             CountryCode countryCode = client.getCountryCodeFromIp(ipAdr);
             if (countryCode == null) {
                 throw new TaskAppException("Country code not found for IP " + ipAdr.address());
             }
             return countryCode;
         });
-    }
-
-    private void clearCacheIfNeeded() {
-        if (localCache.size() > maxCacheSize) {
-            synchronized (this) {
-                if (localCache.size() > maxCacheSize) {
-                    localCache.clear();
-                }
-            }
-        }
     }
 }
